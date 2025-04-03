96b607a297bd8e7e1b8ef561c7dcdbd6cca475b4
Camilo Ospina <camospinac@outlook.com>
2025-04-02 21:11:22 -0500

    Repaso Encapsulamiento

--- src/aritmetica/Aritmetica.java
+++ src/aritmetica/Aritmetica.java
@@ -1,7 +1,7 @@
 package aritmetica;
 public class Aritmetica {
-     int numero1;
-     int numero2;
+     private int numero1;
+     private int numero2;
 
      public Aritmetica() {
      }
@@ -19,5 +19,21 @@ public class Aritmetica {
      public void restar() {
           var resta = numero1 - numero2;
           System.out.println("La resta es: " + resta);
+     }
+
+     public int getNumero1(){
+          return this.numero1;
+     }
+
+     public void setNumero1(int numero1){
+          this.numero1 = numero1;
+     }
+
+     public int getNumero2(){
+          return this.numero2;
+     }
+
+     public void setNumero2(int numero2){
+          this.numero2 = numero2;
      }
 }
--- src/prueba/PruebaAritmetica.java
+++ src/prueba/PruebaAritmetica.java
@@ -6,6 +6,10 @@ public class PruebaAritmetica {
          public static void main(String[] args) {
           System.out.println("***** Operaciones matematicas desde Prueba *******");
           var calculadora = new Aritmetica(5485754, 4938489);
+
+          calculadora.setNumero1(35);
+          calculadora.setNumero2(10);
+
           calculadora.sumar();
           calculadora.restar();
      }
